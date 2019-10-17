c960b8f1add9a835e7bcbbb278f2ab63cd302789
Ederson Machado <edersonmberti@gmail.com>
2019-10-17 19:35:15 -0300

    first animation

--- src/screens/login/login.screen.js
+++ src/screens/login/login.screen.js
@@ -1,11 +1,46 @@
-import React from 'react';
-import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Animated,
+  Easing,
+} from 'react-native';
 
 import Styles from './login.style';
 
 export function LoginScreen() {
+  const [fadeAnim] = useState(new Animated.Value(0));
+  const [RotateValueHolder] = useState(new Animated.Value(0));
+
+  useEffect(() => {
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 3000,
+    }).start();
+    StartImageRotateFunction();
+  }, []);
+
+  function StartImageRotateFunction() {
+    Animated.timing(RotateValueHolder, {
+      toValue: 1,
+      duration: 3000,
+      easing: Easing.linear,
+    }).start();
+  }
+
+  const RotateData = RotateValueHolder.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['0deg', '360deg'],
+  });
+
   return (
-    <View style={Styles.container}>
+    <Animated.View
+      style={[
+        Styles.container,
+        { opacity: fadeAnim, transform: [{ rotate: RotateData }] },
+      ]}>
       <TextInput
         style={Styles.input}
         placeholder="Username"
@@ -23,6 +58,6 @@ export function LoginScreen() {
       <TouchableOpacity style={Styles.button}>
         <Text style={Styles.buttonText}>LOGIN</Text>
       </TouchableOpacity>
-    </View>
+    </Animated.View>
   );
 }
