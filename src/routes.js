b417d7405c8a8e755cfa8ac18d5b7cb46acbbbcb
Ederson Machado <edersonmberti@gmail.com>
2019-11-05 13:11:35 -0300

    add package config

--- src/routes.js
+++ src/routes.js
@@ -3,33 +3,33 @@ import { createStackNavigator } from 'react-navigation-stack';
 
 import { Home, Fade, Rotate, Lottie } from './screens';
 
-const Routes = createAppContainer(
-  createStackNavigator({
-    Home: {
-      screen: Home,
-      navigationOptions: {
-        title: 'Home',
-      },
+export const animatedStack = createStackNavigator({
+  Home: {
+    screen: Home,
+    navigationOptions: {
+      title: 'Home',
     },
-    Fade: {
-      screen: Fade,
-      navigationOptions: {
-        title: 'Fade',
-      },
+  },
+  Fade: {
+    screen: Fade,
+    navigationOptions: {
+      title: 'Fade',
     },
-    Rotate: {
-      screen: Rotate,
-      navigationOptions: {
-        title: 'Rotate',
-      },
+  },
+  Rotate: {
+    screen: Rotate,
+    navigationOptions: {
+      title: 'Rotate',
     },
-    Lottie: {
-      screen: Lottie,
-      navigationOptions: {
-        title: 'Lottie',
-      },
+  },
+  Lottie: {
+    screen: Lottie,
+    navigationOptions: {
+      title: 'Lottie',
     },
-  }),
-);
+  },
+});
+
+const Routes = createAppContainer(animatedStack);
 
 export default Routes;
